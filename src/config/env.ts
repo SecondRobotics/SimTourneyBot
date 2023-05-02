3c600553d3d3f9b380ba650e0d5432bbeeb30e9b
Nicholas Bottone <nick@bottone.io>
2023-05-02 15:57:09 -0400

    Modify env checker

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,12 +4,13 @@ import { z } from "zod";
 dotenv.config();
 
 const envSchema = z.object({
-  GOOGLE_SHEET_CLIENT_EMAIL: z.string(),
-  GOOGLE_SHEET_PRIVATE_KEY: z.string(),
-  GOOGLE_SHEET_DOC_ID: z.string(),
-  DISCORD_TOKEN: z.string(),
-  DISCORD_GUILD_ID: z.string(),
-  DISCORD_CHANNEL_ID: z.string(),
+  GOOGLE_SHEET_CLIENT_EMAIL: z.string().email(),
+  GOOGLE_SHEET_PRIVATE_KEY: z.string().min(1),
+  GOOGLE_SHEET_DOC_ID: z.string().min(1),
+  DISCORD_TOKEN: z.string().min(1),
+  DISCORD_CLIENT_ID: z.string().min(1),
+  DISCORD_GUILD_ID: z.string().min(1),
+  DISCORD_CHANNEL_ID: z.string().min(1),
 });
 
 envSchema.parse(process.env);
